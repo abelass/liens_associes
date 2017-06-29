04154ab5e97cbc6e9d3b975cebda579d0f576025
abelass <rainer@websimple.be>
2017-06-29 13:32:54 +0200

    php doc

--- la_recherche_objet_fonctions.php
+++ la_recherche_objet_fonctions.php
@@ -1,19 +1,29 @@
 <?php
 
+/**
+ * Retrourne un json de resultats d'une requete jquer autocmplete.
+ * @param string $table
+ *    le nom de l table
+ * @return string Json objet contenat label et data.
+ */
 function la_recherche_objets($table) {
-	// Le sonnées de l'objet.
+
+	// Le données de l'objet.
+	$champ_id_table = id_table_objet($table);
 	$trouver_table = charger_fonction('trouver_table', 'base');
 	$desc = $trouver_table($table);
-	$id_table = $desc['key']['PRIMARY KEY'];
-	$titre = explode(',', $desc['titre']);
 
-	$champ_titre= $titre[0];
-	$champs = array($id_table,$champ_titre);
+	// Chercher le champ du titre, il y surement mieux.
+	$titre = explode(',', $desc['titre']);
+	$titre = explode('AS', $titre[0]);
+	$champ_titre = trim($titre[0]);
+
+	// LA requête
+	$champs = array($champ_id_table,$champ_titre);
 	$where = array(
 		'statut LIKE "publie"',
 		$champ_titre . ' LIKE ' . sql_quote('%' . _request('term') . '%')
 	);
-
 
 	// La requête.
 	$sql = sql_select($champs, $table, $where);
@@ -21,7 +31,7 @@ function la_recherche_objets($table) {
 	while ($data = sql_fetch($sql)) {
 		$objets[] = array(
 			'label' => $data[$champ_titre],
-			'data' => $data[$id_table],
+			'data' => $data[$champ_id_table],
 			);
 	}
 
